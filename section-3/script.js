d4b54b05768d85128e03283e34c681cc09253376
negarlatifian <74406918+negarlatifian@users.noreply.github.com>
2022-08-16 13:41:16 +0100

    section-03 drawing rectangle with mouse clicks

--- section-3/script.js
+++ section-3/script.js
@@ -3,8 +3,15 @@ const canvas = document.getElementById("my-canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
-console.log(ctx);
-ctx.beginPath();
-ctx.rect(100, 100, 100, 100);
-// ctx.fill();
-ctx.stroke();
+// console.log(ctx);
+// ctx.beginPath();
+// ctx.rect(100, 100, 100, 100);
+// // ctx.fill();
+// ctx.stroke();
+
+canvas.addEventListener("click", (event) => {
+  //   console.log(event.x, event.y);
+  ctx.beginPath();
+  ctx.rect(event.x, event.y, 10, 10);
+  ctx.fill();
+});
