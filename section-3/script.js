ea2b4ed69428c49f0ae70095e64082483dd6c000
negarlatifian <74406918+negarlatifian@users.noreply.github.com>
2022-08-17 15:14:00 +0100

    section-8 generate starry night effect done

--- section-3/script.js
+++ section-3/script.js
@@ -24,17 +24,63 @@ const ctx = canvas.getContext("2d");
 // ctx.arc(100, 100, 50, 0, degToRad(180));
 // ctx.fill();
 
-// -------------------------6. Generate Atoms -------------------
+// // -------------------------6. Generate Atoms -------------------
+
+// let atoms = [];
+// canvas.addEventListener("mousemove", (event) => {
+//   for (let i = 0; i < 50; i++) {
+//     atoms.push(new Atom(event.x, event.y));
+//   }
+// });
+
+// const animate = () => {
+//   atoms.forEach((atom, index) => {
+//     atom.draw();
+//     atom.updateSpeed();
+//     atom.updateRadius();
+
+//     if (atom.radius < 0.3) {
+//       atoms.splice(index, 1);
+//     }
+//   });
+//   ctx.save();
+//   ctx.fillStyle = "rgba(255, 255, 255, 0.2)";
+//   ctx.fillRect(0, 0, canvas.width, canvas.height);
+//   ctx.restore();
+//   requestAnimationFrame(animate);
+// };
+
+// class Atom {
+//   constructor(x, y) {
+//     this.x = x;
+//     this.y = y;
+//     this.radius = Math.random() * 8 + 2;
+//     this.speedX = Math.random() * 4 - 2; // bewtween -2 and +2
+//     this.speedY = Math.random() * 4 - 2; // bewtween -2 and +2
+//   }
+//   updateSpeed() {
+//     this.x += this.speedX;
+//     this.y += this.speedY;
+//   }
+
+//   updateRadius() {
+//     this.radius -= 0.1;
+//   }
+
+//   draw() {
+//     ctx.beginPath();
+//     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+//     ctx.fill();
+//   }
+// }
+// animate();
+// -------------------------8. Generate Sky star effect -------------------
 
 let atoms = [];
-canvas.addEventListener("mousemove", (event) => {
-  for (let i = 0; i < 10; i++) {
-    atoms.push(new Atom(event.x, event.y));
-  }
-});
 
 const animate = () => {
   atoms.forEach((atom, index) => {
+    ctx.fillStyle = "white";
     atom.draw();
     atom.updateSpeed();
     atom.updateRadius();
@@ -44,17 +90,18 @@ const animate = () => {
     }
   });
   ctx.save();
-  ctx.fillStyle = "rgba(255, 255, 255, 0.2)";
+  ctx.fillStyle = "rgba(0, 0, 0, 0.2)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   ctx.restore();
   requestAnimationFrame(animate);
 };
+animate();
 
 class Atom {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.radius = Math.random() * 8 + 2;
+    this.radius = Math.random() * 2 + 2;
     this.speedX = Math.random() * 4 - 2; // bewtween -2 and +2
     this.speedY = Math.random() * 4 - 2; // bewtween -2 and +2
   }
@@ -73,4 +120,10 @@ class Atom {
     ctx.fill();
   }
 }
-animate();
+const generateAtoms = () => {
+  atoms.push(
+    new Atom(Math.random() * canvas.width, Math.random() * canvas.height)
+  );
+  requestAnimationFrame(generateAtoms);
+};
+generateAtoms();
