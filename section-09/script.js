a2938e2b075684f4c58e865cdfe20dcff2d617a8
negarlatifian <74406918+negarlatifian@users.noreply.github.com>
2022-08-17 19:58:44 +0100

    section-09 image is showing

--- section-09/script.js
+++ section-09/script.js
@@ -1,73 +1,11 @@
 const canvas = document.getElementById("my-canvas");
-// canvas is not full screen by default
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
 
-// -------------------------8. Generate Sky star effect -------------------
+const img = new Image();
+img.src = "photo.png";
 
-let atoms = [];
-
-const animate = () => {
-  atoms.forEach((atom, index) => {
-    ctx.fillStyle = "white";
-    atom.draw();
-    atom.updateSpeed();
-    atom.updateRadius();
-
-    if (atom.radius < 0.3) {
-      atoms.splice(index, 1);
-    }
-  });
-  ctx.save();
-  ctx.fillStyle = "rgba(0, 0, 0, 0.2)";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.restore();
-  requestAnimationFrame(animate);
+img.onload = () => {
+  canvas.width = img.width / 3;
+  canvas.height = img.height / 3;
+  ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 };
-animate();
-
-class Atom {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.radius = Math.random() * 2 + 2;
-    this.speedX = Math.random() * 4 - 2; // bewtween -2 and +2
-    this.speedY = Math.random() * 4 - 2; // bewtween -2 and +2
-  }
-  updateSpeed() {
-    this.x += this.speedX;
-    this.y += this.speedY;
-  }
-
-  updateRadius() {
-    this.radius -= 0.1;
-  }
-
-  draw() {
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    ctx.fill();
-  }
-}
-
-const point = {
-  x: 0,
-  y: 0,
-};
-
-let degree = 0;
-
-const generateAtoms = () => {
-  atoms.push(
-    new Atom(
-      canvas.width / 2 + point.x * 200,
-      canvas.height / 2 + point.y * 200
-    )
-  );
-  point.x = Math.cos((degree / 180) * Math.PI);
-  point.y = point.x * point.x * point.x;
-  degree++;
-  requestAnimationFrame(generateAtoms);
-};
-generateAtoms();
