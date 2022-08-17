841ad319612a9ccff8f345c0235e36a5a1292b03
negarlatifian <74406918+negarlatifian@users.noreply.github.com>
2022-08-17 13:22:52 +0100

    section-6 generating atoms done

--- section-3/script.js
+++ section-3/script.js
@@ -15,9 +15,50 @@ const ctx = canvas.getContext("2d");
 //   ctx.rect(event.x, event.y, 10, 30);
 //   ctx.fill();
 // });
-const degToRad = (deg) => {
-  return (deg / 180) * Math.PI;
+
+// -------------------------5. Drawing Circle -------------------
+// const degToRad = (deg) => {
+//   return (deg / 180) * Math.PI;
+// };
+// ctx.beginPath();
+// ctx.arc(100, 100, 50, 0, degToRad(180));
+// ctx.fill();
+
+// -------------------------6. Generate Atoms -------------------
+
+let atoms = [];
+canvas.addEventListener("click", (event) => {
+  for (let i = 0; i < 20; i++) {
+    atoms.push(new Atom(event.x, event.y));
+    console.log(atoms);
+  }
+});
+
+const animate = () => {
+  atoms.forEach((atom) => {
+    atom.draw();
+    atom.update();
+  });
+  requestAnimationFrame(animate);
 };
-ctx.beginPath();
-ctx.arc(100, 100, 50, 0, degToRad(180));
-ctx.fill();
+
+class Atom {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.radius = Math.random() * 8 + 2;
+    this.speedX = Math.random() * 4 - 2; // bewtween -2 and +2
+    this.speedY = Math.random() * 4 - 2; // bewtween -2 and +2
+  }
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+animate();
